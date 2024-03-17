91c489d3069586883b4b4f49ee55a0d9f118a59a
Ucopyrightable <132915653+Ucopyrightable@users.noreply.github.com>
2024-03-17 08:42:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,18 +14,18 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '1i src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '2i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
-sed -i '3i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '4i src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
+#sed -i '1i src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '2i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+#sed -i '3i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#sed -i '4i src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
 
 #Add Openclash
-mkdir package/luci-app-openclash
-cd package/luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull origin master
-git branch --set-upstream-to=origin/master master
-cd ../..
+#mkdir package/luci-app-openclash
+#cd package/luci-app-openclash
+#git init
+#git remote add -f origin https://github.com/vernesong/OpenClash.git
+#git config core.sparsecheckout true
+#echo "luci-app-openclash" >> .git/info/sparse-checkout
+#git pull origin master
+3git branch --set-upstream-to=origin/master master
+#cd ../..
