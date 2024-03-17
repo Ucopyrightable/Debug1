45e8bb074b3a9b2203de07ca6eef11b691c92f64
Ucopyrightable <132915653+Ucopyrightable@users.noreply.github.com>
2024-03-17 21:53:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,12 +20,13 @@
 #sed -i '4i src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
 
 #Add Openclash
-#mkdir package/luci-app-openclash
-#cd package/luci-app-openclash
-#git init
-#git remote add -f origin https://github.com/vernesong/OpenClash.git
-#git config core.sparsecheckout true
-#echo "luci-app-openclash" >> .git/info/sparse-checkout
-#git pull origin master
-#git branch --set-upstream-to=origin/master master
-#cd ../..
+mkdir package/modify
+mkdir package/modify/openclash
+cd package/modify/openclash
+git init
+git remote add origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
+cd ../../..
